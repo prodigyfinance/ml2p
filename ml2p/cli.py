fcec568d1146b527a126497fe448084a3f048cb7
Swartacus <adam.swart@prodigyfinance.com>
2019-06-04 12:22:43 +0200

    Create command group for repos and move helper functions to a new module.

--- ml2p/cli.py
+++ ml2p/cli.py
@@ -2,137 +2,15 @@
 
 """ CLI for Minimal Lovable Machine Learning Pipeline. """
 
-import base64
-import datetime
-import json
-import re
 import urllib.parse
 
 import boto3
 import click
 import yaml
 
+import cli_utils
 
-def date_to_string_serializer(value):
-    """ JSON serializer for datetime objects. """
-    if isinstance(value, datetime.datetime):
-        return str(value)
-    raise TypeError()
-
-
-def click_echo_json(response):
-    """ Echo JSON via click.echo. """
-    click.echo(json.dumps(response, indent=2, default=date_to_string_serializer))
-
-
-def endpoint_url_for_arn(endpoint_arn):
-    """ Return the URL for an endpoint ARN. """
-    match = re.match(
-        r"^arn:aws:sagemaker:(?P<region>[^:]*):(?P<account>[^:]*):"
-        r"endpoint/(?P<endpoint>.*)$",
-        endpoint_arn,
-    )
-    if not match:
-        return None
-    return (
-        "https://runtime.sagemaker.{region}.amazonaws.com/"
-        "endpoints/{endpoint}/invocations".format(**match.groupdict())
-    )
-
-
-def mk_training_job(prj, training_job, dataset):
-    """ Return training job creation parameters. """
-    model_path = prj.s3.url("/models/")
-    train_path = prj.s3.url("/datasets/" + dataset)
-    return {
-        "TrainingJobName": prj.full_job_name(training_job),
-        "AlgorithmSpecification": {
-            "TrainingImage": prj.train.image,
-            "TrainingInputMode": "File",
-        },
-        # training shouldn't make network calls
-        "EnableNetworkIsolation": True,
-        "HyperParameters": {},
-        "InputDataConfig": [
-            {
-                "ChannelName": "training",
-                "DataSource": {
-                    "S3DataSource": {"S3DataType": "S3Prefix", "S3Uri": train_path}
-                },
-            }
-        ],
-        "OutputDataConfig": {"S3OutputPath": model_path},
-        "ResourceConfig": {
-            "InstanceCount": 1,
-            "InstanceType": prj.train.instance_type,
-            "VolumeSizeInGB": 20,
-        },
-        "RoleArn": prj.train.role,
-        "StoppingCondition": {"MaxRuntimeInSeconds": 60 * 60},
-        "Tags": prj.tags(),
-        # TODO: specify security groups
-        # "VpcConfig": {"SecurityGroupIds": ["XXX"], "Subnets": ["XXX"]},
-    }
-
-
-def mk_model(prj, model_name, training_job):
-    """ Return model creation parameters. """
-    model_path = prj.s3.url("/models")
-    model_tgz_path = (
-        model_path + "/" + prj.full_job_name(training_job) + "/output/model.tar.gz"
-    )
-    return {
-        "ModelName": prj.full_job_name(model_name),
-        "PrimaryContainer": {
-            "Image": prj.deploy.image,
-            "ModelDataUrl": model_tgz_path,
-            "Environment": {"ML2P_MODEL_VERSION": prj.full_job_name(model_name)},
-        },
-        "ExecutionRoleArn": prj.deploy.role,
-        "Tags": prj.tags(),
-        # TODO: specify security groups
-        # "VpcConfig": {"SecurityGroupIds": ["XXX"], "Subnets": ["XXX"]},
-        "EnableNetworkIsolation": False,
-    }
-
-
-def mk_endpoint_config(prj, endpoint_name, model_name):
-    """ Return endpoint config creation parameters. """
-    return {
-        "EndpointConfigName": prj.full_job_name(endpoint_name) + "-config",
-        "ProductionVariants": [
-            {
-                "VariantName": prj.full_job_name(model_name) + "-variant-1",
-                "ModelName": prj.full_job_name(model_name),
-                "InitialInstanceCount": 1,
-                "InstanceType": prj.deploy.instance_type,
-                "InitialVariantWeight": 1.0,
-            }
-        ],
-        "Tags": prj.tags(),
-    }
-
-
-def mk_notebook(prj, notebook_name):
-    """ Return a notebook configuration. """
-    return {
-        "NotebookInstanceName": prj.full_job_name(notebook_name),
-        "InstanceType": prj.notebook.instance_type,
-        "RoleArn": prj.notebook.role,
-        "Tags": prj.tags(),
-        "LifecycleConfigName": prj.full_job_name(notebook_name) + "-lifecycle-config",
-        "VolumeSizeInGB": prj.notebook.volume_size,
-    }
-
-
-def mk_notebook_instance_lifecycle_config(prj, notebook_name, on_start):
-    """ Return a notebook instance lifecycle configuration. """
-    on_start = base64.b64encode(on_start.encode("utf-8")).decode("utf-8")
-    return {
-        "NotebookInstanceLifecycleConfigName": prj.full_job_name(notebook_name)
-        + "-lifecycle-config",
-        "OnStart": [{"Content": on_start}],
-    }
+from cli_utils import click_echo_json
 
 
 class ModellingProject:
@@ -146,6 +24,7 @@ class ModellingProject:
         self.train = ModellingSubCfg(self.cfg, "train")
         self.deploy = ModellingSubCfg(self.cfg, "deploy")
         self.notebook = ModellingSubCfg(self.cfg, "notebook")
+        self.repo = ModellingSubCfg(self.cfg, "repo")
 
     def full_job_name(self, job_name):
         return "{}-{}".format(self.cfg["project"], job_name)
@@ -246,7 +125,7 @@ def training_job_list(prj):
 def training_job_create(prj, training_job, dataset):
     """ Create a training job.
     """
-    training_job_params = mk_training_job(prj, training_job, dataset)
+    training_job_params = cli_utils.mk_training_job(prj, training_job, dataset)
     response = prj.client.create_training_job(**training_job_params)
     click_echo_json(response)
 
@@ -300,7 +179,7 @@ def model_list(prj):
 def model_create(prj, model_name, training_job):
     """ Create a model.
     """
-    model_params = mk_model(prj, model_name, training_job)
+    model_params = cli_utils.mk_model(prj, model_name, training_job)
     response = prj.client.create_model(**model_params)
     click_echo_json(response)
 
@@ -350,7 +229,9 @@ def endpoint_list(prj):
 def endpoint_create(prj, endpoint_name, model_name):
     """ Create an endpoint for a model.
     """
-    endpoint_config_params = mk_endpoint_config(prj, endpoint_name, model_name)
+    endpoint_config_params = cli_utils.mk_endpoint_config(
+        prj, endpoint_name, model_name
+    )
     prj.client.create_endpoint_config(**endpoint_config_params)
     response = prj.client.create_endpoint(
         EndpointName=prj.full_job_name(endpoint_name),
@@ -385,7 +266,7 @@ def endpoint_describe(prj, endpoint_name):
     response = prj.client.describe_endpoint(
         EndpointName=prj.full_job_name(endpoint_name)
     )
-    response["EndpointUrl"] = endpoint_url_for_arn(response["EndpointArn"])
+    response["EndpointUrl"] = cli_utils.endpoint_url_for_arn(response["EndpointArn"])
     click_echo_json(response)
 
 
@@ -444,13 +325,13 @@ def notebook_create(prj, notebook_name, on_start_path):
     """
     with open(on_start_path, "r") as f:
         on_start = f.read()
-    notebook_instance_lifecycle_config = mk_notebook_instance_lifecycle_config(
+    notebook_instance_lifecycle_config = cli_utils.mk_notebook_instance_lifecycle_config(
         prj, notebook_name, on_start
     )
     prj.client.create_notebook_instance_lifecycle_config(
         **notebook_instance_lifecycle_config
     )
-    notebook_config = mk_notebook(prj, notebook_name)
+    notebook_config = cli_utils.mk_notebook(prj, notebook_name)
     prj.client.create_notebook_instance(**notebook_config)
 
 
@@ -526,3 +407,19 @@ def notebook_start(prj, notebook_name):
     prj.client.start_notebook_instance(
         NotebookInstanceName=prj.full_job_name(notebook_name)
     )
+
+
+@ml2p.group("repo")
+def notebook():
+    """ Create and manage code repositories. """
+
+
+@repo.command("create")
+@click.argument("repo-name")
+@pass_prj
+def repo_create(prj, repo_name):
+    """ Create a code repository to associate with a notebook instance.
+    """
+    repo_params = cli_utils.mk_repo(prj, repo_name)
+    response = prj.client.create_code_repository(**repo_params)
+    click_echo_json(response)
