d6f264694aeff7640470253956618d5d94da9f2b
Swartacus <adam.swart@prodigyfinance.com>
2019-06-21 11:20:53 +0200

    Make SubnetId and SecurityGroupIds optional.

--- ml2p/cli_utils.py
+++ ml2p/cli_utils.py
@@ -115,8 +115,6 @@ def mk_notebook(prj, notebook_name, repo_name=None):
     notebook_params = {
         "NotebookInstanceName": prj.full_job_name(notebook_name),
         "InstanceType": prj.notebook.instance_type,
-        "SubnetId": prj.notebook.subnet_id,
-        "SecurityGroupIds": prj.notebook.security_group_ids,
         "RoleArn": prj.notebook.role,
         "Tags": prj.tags(),
         "LifecycleConfigName": prj.full_job_name(notebook_name) + "-lifecycle-config",
@@ -124,6 +122,10 @@ def mk_notebook(prj, notebook_name, repo_name=None):
     }
     if repo_name is not None:
         notebook_params["DefaultCodeRepository"] = prj.full_job_name(repo_name)
+    if prj.get("subnet_id"):
+        notebook_params["SubnetId"] = prj.notebook.subnet_id
+    if prj.get("security_group_ids"):
+        notebook_params["SecurityGroupIds"] = prj.notebook.security_group_ids
     return notebook_params
 
 
