e8471df422c050e8961df62735c1238f428a60ce
Simon Cross <hodgestar@gmail.com>
2019-08-27 15:31:04 +0200

    Simplify requirements files and add setup.py to tox linting.

--- setup.py
+++ setup.py
@@ -1,50 +1,43 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="ml2p",
     version="0.0.5",
-    url='http://github.com/prodigyfinance/ml2p',
-    license='Unknown',
+    url="http://github.com/prodigyfinance/ml2p",
+    license="Unknown",
     description=(
-        "A minimum-lovable machine-learning pipeline, built on top of"
-        " AWS SageMaker."
+        "A minimum-lovable machine-learning pipeline, built on top of AWS SageMaker."
     ),
-    long_description=open('README.rst', 'r').read(),
-    author='Prodigy Finance',
-    author_email='devops@prodigyfinance.com',
+    long_description=open("README.rst", "r").read(),
+    author="Prodigy Finance",
+    author_email="devops@prodigyfinance.com",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'boto3',
-        'click',
-        'flask',
-        'flask-API',
-        'PyYAML',
-    ],
+    install_requires=["boto3", "click", "flask", "flask-API", "PyYAML"],
     extras_require={
-        'dev': [
-            'bumpversion',
-            'coverage',
-            'flake8',
-            'pytest',
-            'pytest-cov',
-            'tox',
-        ],
+        "dev": [
+            "black",
+            "bumpversion",
+            "coverage",
+            "flake8",
+            "isort",
+            "pytest",
+            "pytest-cov",
+            "radon",
+            "tox",
+        ]
     },
     entry_points={
-        'console_scripts': [
-            'ml2p=ml2p.cli:ml2p',
-            'ml2p-docker=ml2p.docker:ml2p_docker',
-        ],
+        "console_scripts": ["ml2p=ml2p.cli:ml2p", "ml2p-docker=ml2p.docker:ml2p_docker"]
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
