3869cdf82cf817de75c7b56a9f49542b0df2eee0
Simon Cross <hodgestar@gmail.com>
2019-08-29 15:30:51 +0200

    Bump version: 0.0.5 → 0.0.6

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2019, Prodigy Finance"
 author = "Prodigy Finance"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.5"
+release = "0.0.6"
 # The short X.Y version.
 version = "{ver[0]}.{ver[1]}".format(ver=release.split("."))
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ml2p",
-    version="0.0.5",
+    version="0.0.6",
     url="http://github.com/prodigyfinance/ml2p",
     license="Unknown",
     description=(
