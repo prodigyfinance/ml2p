3b7dc6b351858e4a962bc6a9c1b8afc9d543788e
Simon Cross <hodgestar@gmail.com>
2020-01-23 13:15:31 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ml2p",
-    version="0.1.1",
+    version="0.1.2",
     url="http://github.com/prodigyfinance/ml2p",
     license="Unknown",
     description=(
