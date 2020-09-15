150d74f9e7471419e9a8f0f380a5cd90aeb6209b
Simon Cross <hodgestar@gmail.com>
2020-09-16 00:30:01 +0200

    Add sphinx_click plugin.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,12 @@ version = "{ver[0]}.{ver[1]}".format(ver=release.split("."))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx_click",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
