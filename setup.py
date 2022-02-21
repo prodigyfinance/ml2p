a0679d4da2693d7aede5cd68d049115ef9dfa89f
Kyle Harrison <kyle.harrison@prodigyfinance.com>
2022-02-21 12:24:27 +0200

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ml2p",
-    version="0.2.3",
+    version="0.2.4",
     url="http://github.com/prodigyfinance/ml2p",
     license="ISCL",
     description=(
