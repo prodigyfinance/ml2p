80f97c18ef63e9d6e0a2dec0850a88fc231e3f85
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-02-14 18:19:44 +0200

    Update python version

--- setup.py
+++ setup.py
@@ -39,8 +39,7 @@ setup(
         "Operating System :: POSIX",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
