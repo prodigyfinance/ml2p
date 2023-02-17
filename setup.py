8a4e635d0a9d60c152914212d69e094546497426
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-02-17 18:02:48 +0200

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def_requirements = [
 
 setup(
     name="ml2p",
-    version="0.3.1",
+    version="0.3.2",
     url="http://github.com/prodigyfinance/ml2p",
     license="ISCL",
     description=(
